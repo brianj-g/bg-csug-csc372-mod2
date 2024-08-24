cd486c54b1663555f5dfa18298c4debc73b764cd
Brian Gunther <brian.gunther@outlook.com>
2024-08-24 10:33:17 -0700

    AccountManager r0

---
+++ src/AccountManager.java
@@ -0,0 +1,18 @@
+/*
+ * Module 2 CTA Option 1: Creating a GUI Bank Balance Application
+ * Brian Gunther
+ * CSC372: Programming II
+ * Colorado State University Global
+ * Dr. Vanessa Cooper
+ * August 18, 2024
+ * 
+ */
+
+public class AccountManager {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,5 +1,5 @@
 /*
- * Module 1 CTA Option #1: Implementing a superclass bank account
+ * Module 2 CTA Option 1: Creating a GUI Bank Balance Application
  * Brian Gunther
  * CSC372: Programming II
  * Colorado State University Global
