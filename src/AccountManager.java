0ad8d0d48dc1376e2664dadb865cd26cbd22035d
Brian Gunther <brian.gunther@outlook.com>
2024-08-24 11:10:44 -0700

    AccountManager r1

--- src/AccountManager.java
+++ src/AccountManager.java
@@ -8,8 +8,57 @@
  * 
  */
 
-public class AccountManager {
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JFormattedTextField;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
+/**
+ * AccountManager class provides GUI and main program for the bank account application
+ */
+public class AccountManager extends JFrame implements ActionListener {
+	private JPanel accountDetailPanel;					// Contains account details components
+	private JPanel textEntryPanel;						// Contains text entry components
+	private JPanel actionPanel;							// Contains application control components
+	private JLabel accountIDLabel;						// Label for the account ID
+	private JLabel accountNameLabel;					// Label for account holder's name
+	private JLabel accountBalanceLabel;					// Label for account balance field
+	private JLabel withdrawalLabel;						// Label for withdrawal field
+	private JLabel depositLabel;						// Label for deposit field
+	private JFormattedTextField withdrawalField;		// Holds withdrawal input
+	private JFormattedTextField depositField;			// Holds deposit input
+	private JTextField accountBalanceField;				// Displays account balance
+	private JButton depositButton;						// Creates event for deposit
+	private JButton withdrawalButton;					// Creates event for withdrawal
+	private JButton exitButton;							// Creates event for program exit
+	
+	/**
+	 * Default constructor for AccountManager class
+	 */
+	public AccountManager(BankAccount account) {
+		GridBagConstraints abpConst = null;				// GBL constraints for accountDetailPanel orientation
+		GridBagConstraints teConst = null;				// GBL constraints for textEntryPanel orientation
+		GridBagConstraints aConst = null;				// GBL constraints for actionPanel orientation
+		
+		this.setTitle("Bank Account Management");
+		
+		
+	}
+	
+	/**
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
